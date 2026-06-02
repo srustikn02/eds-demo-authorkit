af94d91d4c632e6f85d0ff5b8baf98f5ad4fa201
srustikn02 <srustikn02@gmail.com>
2026-06-02 22:30:01 +0530

    footer locales revert

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,7 +1,7 @@
 import { getConfig, getMetadata } from '../../scripts/ak.js';
 import { loadFragment } from '../fragment/fragment.js';
 
-const FOOTER_PATH = '/fragments/nav';
+const FOOTER_PATH = '/fragments/nav/footer';
 
 /**
  * loads and decorates the footer
@@ -10,18 +10,22 @@ const FOOTER_PATH = '/fragments/nav';
 export default async function init(el) {
   const { locale } = getConfig();
   const footerMeta = getMetadata('footer');
-  const path = FOOTER_PATH;
+  const path = footerMeta || FOOTER_PATH;
+   console.log('locale:', locale);
+  console.log('locale.prefix:', locale?.prefix);
+  console.log('footerMeta:', footerMeta);
+  console.log('path:', path);
+  console.log('before fragment path:', `${locale?.prefix || ''}${path}`);
   try {
-    const fragment = await loadFragment(`${path}${locale.prefix}/footer`);
+    const fragment = await loadFragment(`${locale.prefix}${path}`);
+    console.log('after fragment path:', fragment);
     fragment.classList.add('footer-content');
 
     const sections = [...fragment.querySelectorAll('.section')];
 
-    // Last section = copyright + legal bar
     const bottom = sections.pop();
     if (bottom) bottom.classList.add('section-bottom');
 
-    // Remaining first section = brand + social
     const top = sections[0];
     if (top) top.classList.add('section-top');
 
